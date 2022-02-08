a76269a2bf2e02e269c1e78f1f0db19276cca1e1
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-02-08 19:52:36 -0300

    Update ex002.js

--- ex002.js
+++ ex002.js
@@ -5,3 +5,29 @@ var nome = "Gustavo"
 nome //vai aparecer a string Gustavo
 nome = "Paulo"//vai apagar o Gustavo e vai atribuir a variavel nome com o valor Paulo
 nome //aparece como resultado Paulo ao inves de Gustavo 
+
+//--------descobrindo o tipo da variavel(node.js)--------- 
+var n =200
+//undefined
+n
+//200
+typeof n
+//'number'
+n ="Google"
+//'Google'
+typeof n
+//'string'
+typeof 6
+//'number'
+typeof "6,5"
+//'string'
+typeof[]
+//'object'
+typeof {}
+//'object'
+typeof function(){}
+//'function'
+typeof NaN
+//'number'
+typeof null
+//'object'
