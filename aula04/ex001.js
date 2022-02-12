f221a6b0d3994cc787d492a4f0e04175057f8f9f
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-02-12 13:49:34 -0300

    Add files via upload

---
+++ aula04/ex001.js
@@ -0,0 +1,7 @@
+//escrevendo no node.js
+"oi" //node. js vai mostrar oi
+3+2 // node.js vai fazer a conta e mostrar o resultado que nesse caso seria 5
+var nome = "Gustavo"
+nome //vai aparecer a string Gustavo
+nome = "Paulo"//vai apagar o Gustavo e vai atribuir a variavel nome com o valor Paulo
+nome //aparece como resultado Paulo ao inves de Gustavo 
