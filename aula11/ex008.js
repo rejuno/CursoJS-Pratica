a6c5b24f036c3455f9ce057a83cfc31ed3b89f87
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-02-19 13:15:00 -0300

    Add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,14 @@
+//baixando o Node Exec (extensao do visual studio code) e possivel escrever o codigo 
+//no arquivo JavaScript(.js) e apertando f8 e possivel 
+//executar diretamente no node.js sem abrir o terminal 
+//console.log('O console funcionou corretamente!')
+
+//CONDICAO SIMPLES
+var vel = 12
+console.log(`A velocidade do seu carro e ${vel} km/h`)
+if(vel >= 60){
+    console.log('Voce ultrapassou a velocidade permitida. Foi Multado')
+}
+    console.log('Dirija sempre usando o cinto de seguranca!')
+
+
---
+++ aula11/ex009.js
@@ -0,0 +1,15 @@
+//CONDICAO COMPOSTA
+var pais = 'EUA'
+if(pais == 'Brasil'){
+    console.log('Brasileira')
+}
+else{
+    console.log('Estrangeiro')
+}
+//Outro jeito de escrever o codigo anterior
+/*if(pais != 'Brasil'){
+    console.log('Voce e Estrangeiro')
+}
+else{
+    console.log('Voce e Brasileiro')
+}*/
