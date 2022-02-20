268f6a66a6256592ac3b46c0356c4a6a157a898d
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-02-20 19:05:06 -0300

    Add files via upload

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+//SEM  repeticao
+    /*console.log('Tudo bem!')
+    console.log('Tudo bem!')
+    console.log('Tudo bem!')
+    console.log('Tudo bem!')
+    console.log('Tudo bem!')
+    console.log('Tudo bem!')*/
+
+//COM REPETICAO que testa o bloco no COMECO (usando WHILE)
+   /* var c = 1
+    while(c <= 500){
+        console.log(`Passo ${c}`)
+        c++ //c recebe c +1 
+    }*/
+
+// COM REPETICAO que TESTA por ULTIMO e EXECUTA o bloco PRIMEIRO
+    /*var c = 1
+    do{
+        console.log(`Passo ${c}`)
+        c++
+    }while(c<=6)*/
+
+
+
+
+ 
+    
