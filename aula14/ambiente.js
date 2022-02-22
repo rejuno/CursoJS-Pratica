e19086630f36c6ade0054552dcc3eba1055d691e
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-02-22 15:31:34 -0300

    Add files via upload

---
+++ aula14/ambiente.js
@@ -0,0 +1,28 @@
+//usando o WHILE
+    /*var c = 1
+    while(c <= 5){
+        console.log(c)
+        c++
+}*/
+    //--------------------------------------
+//usando DO WHILE
+    /*var c = 1
+    do{
+        console.log(c)
+        c++
+    } while(c <= 5)*/
+    //----------------------------------------
+//usando o FOR
+    /*for(var c=1; c<=5; c++){
+        console.log(c)
+
+    }*/
+
+//---------------------------------------
+
+    console.log('Vai comecar...')
+    for(var c=1; c<=5; c++){
+        console.log(c)
+
+    }
+    console.log('FIM')
